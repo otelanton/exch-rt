145fe9f1e811a28c3bd1f5503b25ee1556adef6f
otelanton <ouzel.anton@gmail.com>
2020-06-22 05:52:50 +0300

    feat(Rates): hashCode(), equals()

--- exchange-rates/src/main/java/com/exchangerates/entities/Rates.java
+++ exchange-rates/src/main/java/com/exchangerates/entities/Rates.java
@@ -14,18 +14,19 @@ import javax.persistence.ManyToOne;
 public class Rates {
 
   @Id
-  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
   @Column(nullable = false)
   private float rate;
   @Column(nullable = false)
   private LocalDate date;
-  @ManyToOne(fetch=FetchType.LAZY)
+  @ManyToOne(fetch = FetchType.LAZY)
   private Currency currency;
-  
-  public Rates(){}
 
-  public Rates(float rate, LocalDate date, Currency currency){
+  public Rates() {
+  }
+
+  public Rates(float rate, LocalDate date, Currency currency) {
     this.rate = rate;
     this.date = date;
     this.currency = currency;
@@ -56,22 +57,44 @@ public class Rates {
   }
 
   // public Currency getCurrency() {
-  //   return currency;
+  // return currency;
   // }
 
   public void setCurrency(Currency currency) {
     this.currency = currency;
   }
-  
+
   @Override
   public String toString() {
-    return "CurrencyRates [currency=" 
-      + currency.getCharCode() 
-      + ", date=" 
-      + date + ", id=" 
-      + id 
-      + ", rate=" 
-      + rate 
-      + "]";
+    return "CurrencyRates [currency=" + currency.getCharCode() + ", date=" + date + ", id=" + id + ", rate=" + rate
+        + "]";
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if(obj == null) 
+      return false;
+    if(this == obj) 
+      return true;
+    if(getClass() != obj.getClass()) 
+      return false;
+    Rates o = (Rates) obj;
+    if(this.rate != o.rate) 
+      return false;
+    if(this.date != o.date) 
+      return false;
+    if(this.currency != o.currency)
+      return false;
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int hashCode = 11;
+    hashCode = (int) (31 * hashCode + this.rate);
+    hashCode = 31 * hashCode + this.id;
+    hashCode = 31 * hashCode + this.date.hashCode();
+    hashCode = 31 * hashCode + this.currency.hashCode();
+    return hashCode;
   }
 }
