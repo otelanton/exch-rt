9f34d8a3f69f60a8d77ba92e5b45e8c5e8be1a87
Anton <ouzel.anton@gmail.com>
2021-04-03 22:15:54 +0300

    added kafka

--- exchange-rates/src/main/java/com/exchangerates/domain/initializer/creators/rate/InitialRatesCreator.java
+++ exchange-rates/src/main/java/com/exchangerates/domain/initializer/creators/rate/InitialRatesCreator.java
@@ -2,15 +2,18 @@ package com.exchangerates.domain.initializer.creators.rate;
 
 import com.exchangerates.domain.Rate;
 import com.exchangerates.domain.dao.DataAccessObject;
+import com.exchangerates.domain.event.NewRateCreatedEvent;
 import com.exchangerates.domain.initializer.CreationExecutor;
 import com.exchangerates.domain.initializer.creators.Creator;
 import com.exchangerates.domain.initializer.factory.RateFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @Qualifier("initial")
@@ -18,16 +21,21 @@ class InitialRatesCreator implements Creator{
 
   private DataAccessObject dataAccessObject;
   private RateFactory factory;
+  private KafkaTemplate<Long, List<NewRateCreatedEvent>> kafkaTemplate;
 
   @Autowired
-  public InitialRatesCreator(DataAccessObject dataAccessObject, RateFactory factory) {
+  public InitialRatesCreator(DataAccessObject dataAccessObject, RateFactory factory, KafkaTemplate<Long, List<NewRateCreatedEvent>> kafkaTemplate) {
     this.dataAccessObject = dataAccessObject;
     this.factory = factory;
+    this.kafkaTemplate = kafkaTemplate;
   }
 
   public List<Rate> create(LocalDate date) {
     List<Rate> rates = CreationExecutor.execute(date, factory, Rate.class);
     dataAccessObject.addAllRates(rates);
+    List<NewRateCreatedEvent> events = rates.stream()
+        .map(rate -> new NewRateCreatedEvent(rate.getCurrency().getCharCode(), rate.getValue())).collect(Collectors.toList());
+    kafkaTemplate.send("rates", events);
     return rates;
   }
 }
